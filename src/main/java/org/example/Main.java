be81e86c6ba72217c255d41f1ffddefeeea44d3c
Them1n <s1nedjkeee@gmail.com>
2024-04-17 00:27:14 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 4;
+        System.out.println(x+y);
+        System.out.println("" +x +"" +y);
+
+        // (101)2 = 1*2^2+0*2^1+1*2^0 = 5;
+        // (11)2 = 1*2^1+1*2^0 = 3;
+        // (111)2 = 1*2^2+1*2^1+1*2^0 = 7;
+        // (2)10 = (10)2
+        // (23)10 = (10111)2
+        // (15) = (1111)2
+        int i = 5;
+        // i = i++; out = 5;
+        // i= i++ + i++; out = 11
+        // i = i++ + ++i; out = 12
+        i = ++i + ++i;
+        System.out.println(i);
+        // through all of this operation is is being viewed clearly logic
+    }
+}
